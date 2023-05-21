fa3474659bc65308ff953c21f4be8c9d84595cb0
Otlwaetse Ramoswaane <ramoswaane@gmail.com>
2023-05-21 17:59:53 +0200

    serv env update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,13 @@
 const {default: mongoose} = require('mongoose');
+const asyncHandler = require('express-async-handler');
 
-const dbConnect = ()=>{
+const dbConnect = asyncHandler(async()=>{
     try {
-        const conn = mongoose.connect(process.env.MONGODBC);
+        const conn = await mongoose.connect(process.env.MONGODBC);
        console.log('database connected successfull'); 
     } catch (err) {
       console.log('database error');
     }
-}
+})
 
 module.exports = dbConnect;
